f0cffb52548d93af233cdb532827384ff0157111
Ann Duong <thuyanannduong@gmail.com>
2023-02-14 11:40:38 -0500

    added lecture notes

--- se-unit-5/lesson-2-events/index.js
+++ se-unit-5/lesson-2-events/index.js
@@ -1,6 +1,28 @@
 let bodyElement = document.body;
 console.log(bodyElement)
 
-// function rollDie(){
-//     return Math.ceil(Math.random() * 6)
-// }
+let dieElement = document.querySelector("#die")
+updateDie()
+
+dieElement.addEventListener('click', updateDie)
+document.addEventListener('keydown', handleKeydown)
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener('click', handleButton)
+})
+
+function updateDie(event){
+    console.log(event)
+    dieElement.innerText = Math.ceil(Math.random() * 6)
+}
+
+function handleKeydown(event){
+    document.querySelector("#key")
+        .innerText = event.key
+}
+
+function handleButton(event){
+    console.log(event.target)
+    event.target.disabled = true
+    document.querySelector("#message")
+        .innerText = `You pressed ${event.target.id}`
+}
---
+++ se-unit-5/lesson-2-events/notes.md
@@ -0,0 +1,20 @@
+# JavaScript Events
+
+- What are JavaScript events?
+- Why are events important in modern web development?
+- What are some of the different _types_ of events we might want to respond to when working with code?
+- What are some example of events in your favorite websites?
+
+## Event-Driven DOM Manipulation
+
+> When [some event] happens, I want to manipulate the DOM [in some way]
+
+## Documentation
+- [`addEventListener()`](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
+- [event types](https://developer.mozilla.org/en-US/docs/Web/Events)
+- [`event` object](https://developer.mozilla.org/en-US/docs/Web/API/Event)
+
+### Practice
+- Building a Dice-rolling app
+- Building a Keyboard tracker
+- Managing events for multiple elements
