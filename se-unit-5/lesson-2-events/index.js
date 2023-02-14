37c63005c0f577d15feba90b9077b06648fb6654
Ann Duong <thuyanannduong@gmail.com>
2023-02-14 11:42:50 -0500

    updated starter code

--- se-unit-5/lesson-2-events/index.js
+++ se-unit-5/lesson-2-events/index.js
@@ -1,28 +1,7 @@
 let bodyElement = document.body;
 console.log(bodyElement)
 
-let dieElement = document.querySelector("#die")
-updateDie()
 
-dieElement.addEventListener('click', updateDie)
-document.addEventListener('keydown', handleKeydown)
-document.querySelectorAll("button").forEach(button => {
-    button.addEventListener('click', handleButton)
-})
-
-function updateDie(event){
-    console.log(event)
-    dieElement.innerText = Math.ceil(Math.random() * 6)
-}
-
-function handleKeydown(event){
-    document.querySelector("#key")
-        .innerText = event.key
-}
-
-function handleButton(event){
-    console.log(event.target)
-    event.target.disabled = true
-    document.querySelector("#message")
-        .innerText = `You pressed ${event.target.id}`
+function rollDie(){
+    console.log(Math.ceil(Math.random() * 6))
 }
