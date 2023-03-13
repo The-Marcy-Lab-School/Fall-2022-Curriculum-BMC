76d88a2bdd826cd345f7dfd809ba2cba0381e47b
benspector-mls <ben@marcylabschool.org>
2023-03-13 15:11:40 -0400

    adding DOM content loaded

--- se-unit-6/lesson-0-intro_to_http/random-quote-cors/index.js
+++ se-unit-6/lesson-0-intro_to_http/random-quote-cors/index.js
@@ -1,17 +1,23 @@
 // Some APIs do not serve cross-origin requests.
 // These APIs follow a strict CORS (Cross Origin Resource Sharing) policy
+const main = async () => {
+    const RANDOM_QUOTE_URL = 'https://zenquotes.io/api/random';
 
-const RANDOM_QUOTE_URL = 'https://zenquotes.io/api/random';
+    const fetchData = async (url) => {
+        try {
+            const response = await fetch(url)
+            const data = await response.json();
+            
+            return data;
+        } catch (error) {
+            return error;
+        }
+    }
 
-const quoteElement = document.getElementById('quote');
+    const quoteElement = document.getElementById('quote');
+    quoteElement.innerText = await fetchData(RANDOM_QUOTE_URL);
 
-try {
-    const response = await fetch(RANDOM_QUOTE_URL)
-    const data = await response.json();
-    
-    console.log(data);
-} catch (error) {
-    console.log(error);
+    // https://javascript.info/fetch-crossorigin
 }
 
-// https://javascript.info/fetch-crossorigin
+document.addEventListener("DOMContentLoaded", main);
