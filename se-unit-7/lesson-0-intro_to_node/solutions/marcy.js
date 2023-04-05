70e6056046e23e4796686de4676751004d4e3090
benspector-mls <ben@marcylabschool.org>
2023-04-05 09:59:21 -0400

    removing unneeded files

--- se-unit-7/lesson-0-intro_to_node/solutions/marcy.js
+++
@@ -1,7 +0,0 @@
-// const exportFromLabFile = require('./lab.js');
-// console.log('content exported from the lab.js file:', exportFromLabFile);
-
-require('./lab.js');
-require('./lab.js');
-require('./lab.js');
-require('./lab.js');
--- se-unit-7/lesson-1-http/practice/node-http-application/index.js
+++
@@ -1,5 +0,0 @@
-const http = require('http')
-const fs   = require('fs')
-const url = require('url');
-
-// Your code here! We've required a few of the libraries you'll need to complete the project. 
