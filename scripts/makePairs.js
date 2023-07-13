a13f29b6dc8880be630fb3ca67701df998377db9
benspector-mls <ben@marcylabschool.org>
2023-07-13 14:14:16 -0400

    using a csv file to supply the roster instead of an array

--- scripts/makePairs.js
+++ scripts/makePairs.js
@@ -1,46 +1,34 @@
+const fs = require('fs');
+
 // Run this program by entering the following into the command line:
 // cd into the scripts folder
-// node makePairs.js <groupSize>
+// node makePairs.js <groupSize> <csv file>
 
 /* 
 process.argv contains the arguments passed to the command line
 0 - node
 1 - this file (makePairs.js)
 2 - the first argument after makePairs.js 
+3 - the roster file
 */
 const groupSize = process.argv[2];
-
-if (!groupSize) {
-  console.log("You need a group size. Try node makePairs 2")
+const rosterFile = process.argv[3];
+if (!groupSize || !rosterFile) {
+  console.log("You need a group size and rosterFile. Try node makePairs 2 roster/summer23capstone.txt")
   process.exit(1);
 }
 
-let roster = [
-  'Amanda',
-  'Arly', 
-  'Casterly',
-  'Chuka',      
-  'Clifford',  
-  'Daniel',
-  'Destiny',    
-  'Duojay',    
-  'Ethan',
-  'Fabrizio',   
-  'Gabriel',
-  'Jacqueline', 
-  'Jahshiek',  
-  'Julian',
-  'Kristen',    
-  'Luis',      
-  'Natalie',
-  'Nayan',      
-  'Nicolas',   
-  'SJ',
-  'Shaina',  
-  'Staceyann',
-  'Trevon', 
-  'Vinny', 
-]
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+const allFileContents = fs.readFileSync(rosterFile, 'utf-8');
+const roster = allFileContents.split(/,\n?/);
 
 // made some manual pairs because of office hours
 let pairs = [];
@@ -60,11 +48,7 @@ console.table(pairs);
 
 
 // HELPERS
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}
+
+
+const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
